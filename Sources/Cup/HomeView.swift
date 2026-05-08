dc5305777ff66eb6856be7c96d11852b30e62004
Felix <felix-schindler@outlook.com>
2026-05-08 20:09:58 +0200

    chore: replace default views with empty home

---
+++ Sources/Cup/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeView.swift
+//  cup-gitea
+//
+//  Created by Felix Schindler on 08.05.26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+	var body: some View {
+		List {
+			Text("Hello")
+		}.navigationTitle("Home")
+	}
+}
