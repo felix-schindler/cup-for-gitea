b9ca9cca494cfa596006fdef192a55493c3974c1
Felix <felix-schindler@outlook.com>
2026-05-08 22:28:38 +0200

    refactor: simplify `Network` class

--- Sources/Cup/Network.swift
+++ Sources/Cup/Network.swift
@@ -10,13 +10,8 @@ import GiteaAPI
 
 @MainActor
 class Network {
-	public let shared = Network()
-	public let client: GiteaClient
-	
-	private init() {
-		self.client = GiteaClient(
-			serverURL: URL(string: "https://git.schindlerfelix.de/")!,
-			token: "210f7287c4a374781d524c88314e639c9ef27e2c"
-		)
-	}
+	public static let shared = GiteaClient(
+		serverURL: URL(string: "https://git.schindlerfelix.de/")!,
+		token: "210f7287c4a374781d524c88314e639c9ef27e2c"
+	)
 }
