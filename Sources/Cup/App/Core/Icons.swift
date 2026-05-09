bc9d86fce25616489ffebb4cc4a7f5d13f65ed63
Felix <felix-schindler@outlook.com>
2026-05-09 15:38:42 +0200

    feat(icons): add helper

---
+++ Sources/Cup/App/Core/Icons.swift
@@ -0,0 +1,14 @@
+//
+//  Icons.swift
+//  cup-gitea
+//
+//  Created by Felix Schindler on 09.05.26.
+//
+
+import Foundation
+
+enum Icons: String {
+	case notifications = "bell"
+	case notificationsUnread = "bell.badge"
+	case repositories = "book.closed.fill"
+}
