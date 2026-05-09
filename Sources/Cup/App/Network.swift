64556cac564502e528d5e3182e1f4c86e3930fee
Felix <felix-schindler@outlook.com>
2026-05-09 11:48:37 +0200

    fix(client): server url with suffix

--- Sources/Cup/App/Network.swift
+++ Sources/Cup/App/Network.swift
@@ -11,7 +11,7 @@ import GiteaAPI
 @MainActor
 class Network {
 	public static let shared = GiteaClient(
-		serverURL: URL(string: "https://git.schindlerfelix.de/")!,
+		serverURL: URL(string: "https://git.schindlerfelix.de/api/v1")!,
 		token: "210f7287c4a374781d524c88314e639c9ef27e2c"
 	)
 }
