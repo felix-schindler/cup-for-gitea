fb93d7615bb4a89752faf3b6b2276e694073f0e2
Felix <felix-schindler@outlook.com>
2026-05-09 19:32:53 +0200

    feat(core): add more icons to helper

--- Sources/Cup/App/Core/Icons.swift
+++ Sources/Cup/App/Core/Icons.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 enum Icons: String {
-	case notifications = "bell"
-	case notificationsUnread = "bell.badge"
+	// MARK: - ContentView
+	case home = "house"
+	case explore = "binoculars"
+	case users = "person"
+	case settings = "gear"
+	case search = "magnifyingglass"
+	// MARK: - HomeView
+	case notifications = "tray"
+	case notificationsUnread = "tray.badge"
 	case repositories = "book.closed.fill"
+	case issues = "dot.circle"
+	case pull_requests = "arrow.trianglehead.merge"
+	case milestones = "signpost.right"
+	case organizations = "building.2"
+	case starred = "star"
+	case subscriptions = "bell"
 }
